d74b7628e25ea415c9747c2b5e99174c5154f374
Roman <stolitsin.2018@mail.ru>
2025-11-17 13:29:55 +0300

    feat/1 task

---
+++ app.js
@@ -0,0 +1,16 @@
+const userProfile = {
+    username:  'developer123',
+    email:  'dev@example.com',
+    age: 25,
+    isLoggedIn:  true ,
+
+}
+
+console.log(userProfile.email);
+
+userProfile.age = 26 ;
+
+userProfile["user location"] = "New York";
+
+console.log(userProfile["user location"]);
+
