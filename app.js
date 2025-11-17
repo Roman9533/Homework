6a4c5823375a4e6750b2c1171bce4a63e1054e9a
Roman <stolitsin.2018@mail.ru>
2025-11-17 14:24:59 +0300

    feat/task 3

--- app.js
+++ app.js
@@ -40,4 +40,17 @@ const car = {
     car.startEngine();
     car.stopEngine();
 
+    const salaries = {
+    ivan: 1200,
+    maria: 1500,
+    petr: 1100,
+    anna: 1600
+};
 
+let totalSum = 0;
+
+for(let person in salaries) {
+    totalSum += salaries[person];
+}
+
+console.log("Сумма всех зарплат:", totalSum);
